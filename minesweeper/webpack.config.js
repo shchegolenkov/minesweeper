de2de6c6602ec4742a31fc5ea42cb44e88aa9777
shchegolenkov <denis.amorales@ya.ru>
2023-05-12 19:26:39 +0300

    feat: configure webpack

---
+++ minesweeper/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  devServer: {
+    watchFiles: ["./src/*"],
+    hot: true,
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      }
+    ],
+  },  
+};
